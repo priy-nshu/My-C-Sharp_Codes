2eac75f24c12337db8241f351a6cc1adf62c3b59
priy-nshu <priyanshudwibed6@gmail.com>
2026-04-08 12:19:59 +0530

    WebApp Added

---
+++ MyCoreWebAPP/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using MyCoreWebAPP.Models;
+using System.Diagnostics;
+
+namespace MyCoreWebAPP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ContentResult MyContent()
+        {
+            return Content("<script> alert('Hi! I am Priyanshu')</script", "text/html");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
