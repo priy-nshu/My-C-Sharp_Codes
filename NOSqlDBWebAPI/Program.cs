6ba3ea822881356ecb10281309e5a3f29a422d26
priyanshu dwibedy <priyanshudwibed6@gmail.com>
2026-04-23 17:15:31 +0530

    23/04/26

--- NOSqlDBWebAPI/Program.cs
+++ NOSqlDBWebAPI/Program.cs
@@ -26,13 +26,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
