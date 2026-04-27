715abb8d090b204865b0de2c4bb720773dddf7d5
priyanshu dwibedy <priyanshudwibed6@gmail.com>
2026-04-27 10:31:41 +0530

    Remove secrets from config

---
+++ ProductsWebApp/Models/Attribute.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace ProductsWebApp.Models
+{
+    public class Attributes
+    {
+        [JsonProperty("processor")]
+        public string Processor { get; set; }
+
+        [JsonProperty("ram")]
+        public string Ram { get; set; }
+
+        [JsonProperty("storage")]
+        public string Storage { get; set; }
+
+        [JsonProperty("displaySize")]
+        public string DisplaySize { get; set; }
+    }
+}
---
+++ ProductsWebApp/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace ProductsWebApp.Models
+{
+    public class Product
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("SubCategory")]
+        public string SubCategory { get; set; }
+
+        [JsonProperty("brand")]
+        public string Brand { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("attributes")]
+        public Attribute? Attributes { get; set; }
+
+        [JsonProperty("variants")]
+        public List<Variant>? Variants { get; set; } = new();
+    }
+}
